39be15b26367e00fceb3b054ae85ccb129d6e659
Surya wana bakti <suryawanabakti18@gmail.com>
2023-05-06 14:58:40 +0800

    First Commit

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="row">
+            <div class="col-lg-8 mb-4 order-0">
+                Users
+            </div>
+        </div>
+    </div>
+@endsection
